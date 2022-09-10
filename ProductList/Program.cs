b25dbd392f48ff5be24ab28f4c169a54f1c6680d
Snipercadet <donibris@gmail.com>
2022-09-10 09:49:07 +0100

    swagger

--- ProductList/Program.cs
+++ ProductList/Program.cs
@@ -15,7 +15,7 @@ builder.Services.AddDbContext<ProdDbContext>(x => x.UseNpgsql(builder.Configurat
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
+//if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
